5f730ccf5bea80b0a059bd644fb3fec1720ed82d
StrongStrawberry <strongStrawberry@sidestream.tech>
2026-07-14 09:13:31 +0200

    chore: cleanup

--- src/ValueRegistry.sol
+++ src/ValueRegistry.sol
@@ -24,14 +24,14 @@ contract ValueRegistry {
     // --- Structs ---
     /// @notice A registered value and its position in the keys array
     struct Value {
-        uint256 pos; // 1-based position in the keys array; 0 means not present
+        uint256 pos; // 1 based position in the keys array; 0 means not present
         int256 val; // The value, WAD-scaled by convention
     }
 
     // --- Storage Variables ---
-    /// @notice Mapping of admin addresses
+    /// @notice Mapping of admin addresses (can manage permissions)
     mapping(address => uint256) public wards;
-    /// @notice Mapping of addresses that can operate this registry
+    /// @notice Mapping of operator addresses (can manage key/value pairs)
     mapping(address => uint256) public buds;
     /// @notice Mapping of registered values
     mapping(bytes32 => Value) internal values;
@@ -50,12 +50,12 @@ contract ValueRegistry {
      */
     event Deny(address indexed usr);
     /**
-     * @notice `usr` was granted permission to manage key/value pairs.
+     * @notice `usr` was granted operator access.
      * @param usr The user address.
      */
     event Kiss(address indexed usr);
     /**
-     * @notice Permission revoked for `usr` to manage key/value pairs.
+     * @notice `usr` operator access was revoked.
      * @param usr The user address.
      */
     event Diss(address indexed usr);
@@ -82,7 +82,6 @@ contract ValueRegistry {
         _;
     }
 
-    /// @notice Initialize the registry with the deployer as admin
     constructor() {
         wards[msg.sender] = 1;
         emit Rely(msg.sender);
@@ -91,7 +90,6 @@ contract ValueRegistry {
     // --- Administration ---
     /// @notice Grant authorization to an address
     /// @param usr The address to be authorized
-    /// @dev Sets wards[usr] to 1 and emits Rely event
     function rely(address usr) external auth {
         wards[usr] = 1;
         emit Rely(usr);
@@ -99,7 +97,6 @@ contract ValueRegistry {
 
     /// @notice Revoke authorization from an address
     /// @param usr The address to be deauthorized
-    /// @dev Sets wards[usr] to 0 and emits Deny event
     function deny(address usr) external auth {
         wards[usr] = 0;
         emit Deny(usr);
@@ -107,7 +104,6 @@ contract ValueRegistry {
 
     /// @notice Add an operator
     /// @param usr The address to add as an operator
-    /// @dev Sets buds[usr] to 1 and emits Kiss event. Operators can manage key/value pairs but cannot modify permissions
     function kiss(address usr) external auth {
         buds[usr] = 1;
         emit Kiss(usr);
@@ -115,7 +111,6 @@ contract ValueRegistry {
 
     /// @notice Remove an operator
     /// @param usr The address to remove as an operator
-    /// @dev Sets buds[usr] to 0 and emits Diss event
     function diss(address usr) external auth {
         buds[usr] = 0;
         emit Diss(usr);
@@ -136,9 +131,6 @@ contract ValueRegistry {
 
     /// @notice Remove a key from the registry
     /// @param key The key to be removed
-    /// @dev Removes the item from the keys array but moves the last element
-    ///      to its place, which can result in keys being out of order
-    /// @dev Reverts if the key is not set (ValueRegistry/invalid-key)
     function removeValue(bytes32 key) external toll {
         Value storage value = values[key];
         uint256 pos = value.pos;
@@ -179,14 +171,13 @@ contract ValueRegistry {
     /// @notice Returns the value for a particular key
     /// @param key The parameter key (ex. OPT_UTIL_WAD)
     /// @return val The value associated with the key
-    /// @dev Reverts for unset keys, so an unset parameter can never be silently read as 0
     function getValue(bytes32 key) external view returns (int256 val) {
         Value storage value = values[key];
         require(value.pos != 0, "ValueRegistry/invalid-key");
         val = value.val;
     }
 
-    /// @notice Returns the list of keys being tracked by the registry
+    /// @return The list of keys being tracked by the registry
     function list() external view returns (bytes32[] memory) {
         return keys;
     }
